e337500843ef3d0b13a833bd38b7308f31efc686
dbczumar <corey.zumar@databricks.com>
2025-08-29 12:36:16 -0700

    fix
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

--- app.py
+++ app.py
@@ -201,4 +201,4 @@ app.include_router(otel_router)
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("app:app", host="0.0.0.0", port=8123, workers=16, log_level="info")
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, workers=16, log_level="info")
